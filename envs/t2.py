35647a273f3d426d8bbfd500ef6c1dae5bbf79bb
Guilherme Dias <guilhediasm@hotmail.com>
2026-02-05 22:20:25 -0300

    Update t2.py

--- envs/t2.py
+++ envs/t2.py
@@ -327,12 +327,12 @@ class T2(BaseTask):
     def _reset_root_states(self, env_ids):
         self.root_states[env_ids] = self.base_init_state
         self.root_states[env_ids, :2] += self.env_origins[env_ids, :2]
-        # self.root_states[env_ids, :2] = apply_randomization(self.root_states[env_ids, :2], self.cfg["randomization"].get("init_base_pos_xy"))
+        self.root_states[env_ids, :2] = apply_randomization(self.root_states[env_ids, :2], self.cfg["randomization"].get("init_base_pos_xy"))
         self.root_states[env_ids, 2] += self.terrain.terrain_heights(self.root_states[env_ids, :2])
         self.root_states[env_ids, 3:7] = quat_from_euler_xyz(
             torch.zeros(len(env_ids), dtype=torch.float, device=self.device),
             torch.zeros(len(env_ids), dtype=torch.float, device=self.device),
-            torch.zeros(len(env_ids), dtype=torch.float, device=self.device),
+            torch.rand(len(env_ids), device=self.device) * (2 * torch.pi),
         )
         self.root_states[env_ids, 7:9] = apply_randomization(
             torch.zeros(len(env_ids), 2, dtype=torch.float, device=self.device),
